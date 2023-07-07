924ce4e92c665b77e7e1de028cc124a9e6a47ada
Santiago Etcheverry <santetchdev@gmail.com>
2023-07-07 12:55:58 -0300

    add home route and remove onboarding route from flow

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,9 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           home: const Welcome(),
+          routes: {
+            "myHomePage": (context) => const MyHomePage(),
+          },
         ),
       ),
     );
--- lib/pages/welcome/views/welcome.dart
+++ lib/pages/welcome/views/welcome.dart
@@ -2,7 +2,6 @@ import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:u_course_example/main.dart';
 import 'package:u_course_example/pages/welcome/bloc/welcome_blocs.dart';
 import 'package:u_course_example/pages/welcome/bloc/welcome_events.dart';
 import 'package:u_course_example/pages/welcome/bloc/welcome_states.dart';
@@ -138,11 +137,13 @@ class _WelcomeState extends State<Welcome> {
                 curve: Curves.decelerate,
               );
             } else {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const MyHomePage(),
-                ),
-              );
+              // Navigator.of(context).push(
+              //   MaterialPageRoute(
+              //     builder: (context) => const MyHomePage(),
+              //   ),
+              // );
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil("myHomePage", (route) => false);
             }
           },
           child: Container(
